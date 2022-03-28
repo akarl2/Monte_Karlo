40598670729b1f110bc48a4a94ef9b6ba2ba8f08
Karl Laptop <andrew.m.karl@gmail.com>
2022-03-28 14:06:09 -0500

    Closer results

--- Monte_Karlo.py
+++ Monte_Karlo.py
@@ -40,9 +40,16 @@ def monte_karlo(n):
                 else:
                     composition.remove(reactant)
                     composition.append("Butanol AC")
+
             elif reactant == "Butanol AC X+1":
-                composition.remove(reactant)
-                composition.append("Butanol AC X+2")
+                reactant = random.choice(composition)
+                if reactant == "Butanol AC X+1":
+                    reactant = random.choice(composition)
+                    composition.remove(reactant)
+                    composition.append("Butanol AC X+2")
+                else:
+                    composition.remove(reactant)
+                    composition.append("Butanol AC X+1")
             else:
                 composition.remove(reactant)
                 composition.append("Butanol AC X+3")
@@ -63,7 +70,7 @@ def monte_karlo(n):
         Butanol_AC_X3.append(d[x])
 
 
-monte_karlo(10000)
+monte_karlo(10001)
 average_Butanol = sum(Butanol) / len(Butanol)
 average_ButanolAC = sum(Butanol_AC) / len(Butanol_AC)
 average_ButanolACX1 = sum(Butanol_AC_X1) / len(Butanol_AC_X1)
@@ -78,11 +85,11 @@ wt_average_butanolACX3 = average_ButanolACX3 * 444.2
 
 total = wt_average_butanol + wt_average_butanolAC + wt_average_butanolACX1 + wt_average_butanolACX2 + wt_average_butanolACX3
 
-print(round(wt_average_butanol / total * 100, 2))
-print(round(wt_average_butanolAC / total * 100, 2))
-print(round(wt_average_butanolACX1 / total * 100, 2))
-print(round(wt_average_butanolACX2 / total * 100, 2))
-print(round(wt_average_butanolACX3 / total * 100, 2))
+print(f"Butanol: {round(wt_average_butanol / total * 100, 2)}")
+print(f"Butanol AC: {round(wt_average_butanolAC / total * 100, 2)}")
+print(f"Butanol AC X+1: {round(wt_average_butanolACX1 / total * 100, 2)}")
+print(f"Butanol AC X+2: {round(wt_average_butanolACX2 / total * 100, 2)}")
+print(f"Butanol AC X+3+: {round(wt_average_butanolACX3 / total * 100, 2)}")
 
 fig = plt.figure()
 ax = fig.add_axes([0, 0, 1, 1])
