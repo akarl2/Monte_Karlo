598e8c0090546b7ebac635c522d88a99e2b926ae
akarl2 <andrew.m.karl@gmail.com>
2022-04-05 06:16:23 -0500

    Everything converted to seconds

--- ChemData.py
+++ ChemData.py
@@ -1,9 +1,11 @@
 ChemData = {
-    "Butanol": {"MW": 74.14, "DEN": 0.81},
-    "Epichlorohydrin": {"MW": 92.52, "DEN": 1.18},
-    "Glacial Acetic Acid": {"MW": 60.052, "DEN": 1.05},
-    "Water": {"MW": 18.01, "DEN": 0.997},
-    "70% H2O2": {"MW": 34.02, "DEN": 1.294},
-    "Peracetic Acid": {"MW": 76.05, "DEN": 1.04},
+    "Butanol": {"MW": 74.14, "DEN": 0.81, "Cp": 183.3},
+    "Epichlorohydrin": {"MW": 92.52, "DEN": 1.18, "Cp": 152},
+    "Glacial Acetic Acid": {"MW": 60.052, "DEN": 1.05, "Cp": 121.3},
+    "Water": {"MW": 18.01, "DEN": 0.997, "Cp": 75.38},
+    "70% H2O2": {"MW": 34.02, "DEN": 1.294, "Cp": 43.20},
+    "Peracetic Acid": {"MW": 76.05, "DEN": 1.04, "Cp": 82.76},
+    "Peraformic Acid": {"MW": 62.02, "DEN": 1.3, "Cp": 99.04},
+    "Formic Acid": {"MW": 46.03, "DEN": 1.22, "Cp": 101.3},
 }
 
--- PAA_Seconds.py
+++ PAA_Seconds.py
@@ -3,17 +3,23 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 from ChemData import ChemData
 
-gA = 15
-gB = 15
+gA = 500
+gB = 500
 gC = 0
-gD = 5
+gD = 250
 
-molesA = gA / ChemData["Glacial Acetic Acid"]["MW"]
-molesB = gB / ChemData["70% H2O2"]["MW"]
-molesC = gC / ChemData["Peracetic Acid"]["MW"]
-molesD = gD / ChemData["Water"]["MW"]
+ngA = gA
+ngB = gB * .7
+ngC = 0
+ngD = gD + (gB*.3)
 
-volume = (gA * ChemData["Glacial Acetic Acid"]["DEN"] + gB * ChemData["70% H2O2"]["DEN"] + gC * ChemData["Peracetic Acid"]["DEN"] + gD * ChemData["Water"]["DEN"]) / 1000
+molesA = ngA / ChemData["Glacial Acetic Acid"]["MW"]
+molesB = ngB / ChemData["70% H2O2"]["MW"]
+molesC = ngC / ChemData["Peracetic Acid"]["MW"]
+molesD = ngD / ChemData["Water"]["MW"]
+
+volume = (gA * ChemData["Glacial Acetic Acid"]["DEN"] + gB * ChemData["70% H2O2"]["DEN"] + gC *
+          ChemData["Peracetic Acid"]["DEN"] + gD * ChemData["Water"]["DEN"]) / 1000
 
 MA = molesA / volume
 MB = molesB / volume
@@ -26,9 +32,11 @@ s_conB = MB
 s_conC = MC
 s_conD = MD
 cat_con = .314
-temperature = 330
-k1 = (6.83*10**8)*(math.sqrt(s_conA*(1.75*10**-5)) + cat_con + math.sqrt(cat_con*(1.3*10**-2)))*(math.exp((-57846.15)/(8.134*temperature)))
-k2 = (6.73*10**8)*(math.sqrt(s_conA*(1.75*10**-5)) + cat_con + math.sqrt(cat_con*(1.3*10**-2)))*(math.exp((-60407.78)/(8.134*temperature)))
+temperature = 320
+k1 = (6.83 * 10 ** 8) * (math.sqrt(s_conA * (1.75 * 10 ** -5)) + cat_con + math.sqrt(cat_con * (1.3 * 10 ** -2))) * (
+    math.exp((-57846.15) / (8.134 * temperature)))
+k2 = (6.73 * 10 ** 8) * (math.sqrt(s_conA * (1.75 * 10 ** -5)) + cat_con + math.sqrt(cat_con * (1.3 * 10 ** -2))) * (
+    math.exp((-60407.78) / (8.134 * temperature)))
 conA = s_conA
 conB = s_conB
 conC = s_conC
@@ -38,38 +46,43 @@ con_A = [conA]
 con_B = [conB]
 con_C = [conC]
 con_D = [conD]
-watts_list = [0]
+watts_list = []
 energy = 0
-enthalpy = -13600
+std_enthalpy = -13700
+rxn_enthalpy = std_enthalpy + (((ChemData["Peracetic Acid"]["Cp"] + ChemData["Water"]["Cp"]) - (
+        ChemData["70% H2O2"]["Cp"]+ ChemData["Glacial Acetic Acid"]["Cp"])) * (temperature - 298.15))
+
 
 while seconds < minutes * 60:
-    rate =(((k1 * conA * conB) - (k2 * conC * conD)) / 60) / 60
+    rate = (((k1 * conA * conB) - (k2 * conC * conD)) / 60) / 60
     conA = conA - rate
     conB = conB - rate
     conC = conC + rate
-    energy = energy + (enthalpy * rate)
     conD = conD + rate
     con_A.append(conA), con_B.append(conB), con_C.append(conC), con_D.append(conD)
     keq = (conC * conD) / (conA * conB)
-    watts = energy * rate * -1
+    molesC = rate * volume
+    print(molesC)
+    energy = energy + (rxn_enthalpy * molesC)
+    watts = molesC * rxn_enthalpy * -1
     watts_list.append(watts)
-    print(seconds, energy, watts)
-    seconds = seconds + 1
+    seconds += 1
+
 
 def plot_conc():
-    plt.xlabel("Time (min)")
+    plt.xlabel("Time (seconds)")
     plt.ylabel("Concentration (mol/L)")
-    plt.xlim(0, minutes*60), plt.ylim(0, 8)
+    plt.xlim(0, minutes * 60), plt.ylim(0, 8)
     plt.plot(con_A), plt.plot(con_B), plt.plot(con_C), plt.plot(con_D)
     plt.show()
+
 
 def plot_watts():
     plt.xlabel("Time (seconds)")
     plt.ylabel("Watts")
-    plt.xlim(0, minutes*60), plt.ylim(0, max(watts_list) + 5)
+    plt.xlim(0, minutes * 60), plt.ylim(0, max(watts_list) * 1.1)
     plt.plot(watts_list)
     plt.show()
 
-plot_watts()
-
+plot_conc()
 
