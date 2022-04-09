31b63b3f0d6bac451f7e9d0093735cfb00edc8b6
akarl2 <andrew.m.karl@gmail.com>
2022-04-09 08:03:31 -0500

    PAA_Seconds Addition

---
+++ GlycerolGE_Kinetics.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+from ChemData import ChemData
+
+Alcohol = 285.52
+Epichlorohydrin = 359.39
+rxn_mass_list = {"A1_E" + str(i): 0 for i in range(1, 26)}
+
+molesA = Alcohol / ChemData["Glycerol"]["MW"]
+molesB = Epichlorohydrin / ChemData["Epichlorohydrin"]["MW"]
+rxn_moles_list = {"A1_E" + str(i): 0 for i in range(1, 26)}
+
+p_mass_list = {"A1_E" + str(i): [] for i in range(1, 26)}
+
+k1 = .00001
+k2 = .0000006
+k3 = 0
+k5 = .00000018
+
+while molesB >= 0.9:
+    rate1 = k1 * molesA * molesB
+    rate2 = k2 * molesC * molesB
+    rate3 = k3 * molesD * molesB
+    rate4 = k4 * molesE * molesB
+    rate5 = k5 * molesA * molesB
+
+    molesA = molesA - rate1
+    molesB = molesB - rate1 - rate2 - rate3 - rate4 - rate5
+    molesC2 = molesC2 + rate5
+    molesC = molesC + rate1 - rate2
+    molesD = molesD + rate2 - rate3
+    molesE = molesE + rate3 - rate4
+    molesF = molesF + rate4
+
+    gA = molesA * ChemData["Butanol"]["MW"]
+    gb = molesB * ChemData["Epichlorohydrin"]["MW"]
+    gC = molesC * ChemData["Butanol AC"]["MW"]
+    gC2 = molesC2 * ChemData["Butanol ACBRXN"]["MW"]
+    gD = molesD * ChemData["Butanol AC2"]["MW"]
+    gE = molesE * ChemData["Butanol AC3"]["MW"]
+    gF = molesF * ChemData["Butanol AC4"]["MW"]
+
+    total = gA + gb + gC + gD + gE + gF + gC2
+    p_A.append(round((gA / total) * 100, 2))
+    p_B.append(round((gB / total) * 100, 2))
+    p_C.append(round((gC / total) * 100, 2))
+    p_C2.append(round((gC2 / total) * 100, 2))
+    p_D.append(round((gD / total) * 100, 2))
+    p_E.append(round((gE / total) * 100, 2))
+    p_F.append(round((gF / total) * 100, 2))
+    print(round(molesB, 4))
+
+
+def plot_conc():
+    plt.figure("Percent composition")
+    plt.xlabel("Reactions")
+    plt.ylabel("Percent (%)")
+    plt.ylim(0, 85)
+    plt.xlim(0, len(p_A))
+    plt.plot(p_A, label="Butanol")
+    plt.plot(p_C)
+    plt.plot(p_C2)
+    plt.plot(p_D)
+    plt.plot(p_E)
+    plt.plot(p_F)
+    plt.show()
+
+print(p_A[-1], p_C[-1], p_C2[-1], p_D[-1], p_E[-1], p_F[-1])
+plot_conc()
---
+++ Glycerol_Esters.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+from ChemData import ChemData
+
+Alcohol = 100
+Oleic_acid = 50
+species = 4
+rxn_mass_dict = {"A1_O" + str(i): 0 for i in range(1, species + 1)}
+
+
+molesA = Alcohol / ChemData["Glycerol"]["MW"]
+molesB = Oleic_acid / ChemData["Oleic Acid"]["MW"]
+rxn_moles_dict = {"A1_0" + str(i): 0 for i in range(1, species + 1)}
+rxn_molar_mass = {"A1_0" + str(i): ChemData["Glycerol"]["MW"] + i * ChemData["Oleic Acid"]["MW"] - 18.01 for i in range(1, species + 1)}
+p_mass_dict = {"A1_0" + str(i): [] for i in range(1, species + 1)}
+
+print(rxn_mass_dict)
+print(rxn_moles_dict)
+print(rxn_molar_mass)
+print(p_mass_dict)
+
+k1 = .0001
+k2 = k1 / 2
+
+while molesB >= 0.001:
+    rate1 = k1 * molesA * molesB
+    rate2 = k2 * molesA * molesB
+
+    #itterate through each item in the rxn_moles_dict and multiply by the rate and multply by the key in rxn_molar_mass and append to p_mass_dict
+    for key in rxn_moles_dict:
+        rxn_moles_dict[key] = rxn_moles_dict[key] + rate1 * rxn_molar_mass[key]
+        p_mass_dict[key].append(rxn_moles_dict[key])
+
+
+    print(rxn_moles_dict)
+
+    p_mol_dict = {"A1_0" + str(i): 0 for i in range(1, species + 1)}
+
+    gA = molesA * ChemData["Butanol"]["MW"]
+    gb = molesB * ChemData["Epichlorohydrin"]["MW"]
+    gC = molesC * ChemData["Butanol AC"]["MW"]
+    gC2 = molesC2 * ChemData["Butanol ACBRXN"]["MW"]
+    gD = molesD * ChemData["Butanol AC2"]["MW"]
+    gE = molesE * ChemData["Butanol AC3"]["MW"]
+    gF = molesF * ChemData["Butanol AC4"]["MW"]
+
+    total = gA + gb + gC + gD + gE + gF + gC2
+    p_A.append(round((gA / total) * 100, 2))
+    p_B.append(round((gB / total) * 100, 2))
+    p_C.append(round((gC / total) * 100, 2))
+    p_C2.append(round((gC2 / total) * 100, 2))
+    p_D.append(round((gD / total) * 100, 2))
+    p_E.append(round((gE / total) * 100, 2))
+    p_F.append(round((gF / total) * 100, 2))
+    print(round(molesB, 4))
+
+
+def plot_conc():
+    plt.figure("Percent composition")
+    plt.xlabel("Reactions")
+    plt.ylabel("Percent (%)")
+    plt.ylim(0, 85)
+    plt.xlim(0, len(p_A))
+    plt.plot(p_A, label="Butanol")
+    plt.plot(p_C)
+    plt.plot(p_C2)
+    plt.plot(p_D)
+    plt.plot(p_E)
+    plt.plot(p_F)
+    plt.show()
+
+print(p_A[-1], p_C[-1], p_C2[-1], p_D[-1], p_E[-1], p_F[-1])
+plot_conc()
