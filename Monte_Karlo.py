32cfff774151ace24809f3d5a1cad7be75295b84
akarl2 <andrew.m.karl@gmail.com>
2022-04-20 00:11:01 -0500

    Predictions are accurate.  Need to make code more general.

--- Monte_Karlo.py
+++ Monte_Karlo.py
@@ -1,104 +1,108 @@
 import collections
 import random
-from Database import *
-from Reactions import *
 import sys
 import tkinter
 import pandas
+from Database import *
+from Reactions import *
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.max_rows', None)
+pandas.set_option('display.width', 100)
 
 #Converts string name to class name
 def str_to_class(classname):
     return getattr(sys.modules[__name__], classname)
 
 #specify reaction chemicals and reaction type
-a = Glycerol()
+a = Sorbitol()
 b = C181()
 rt = Esterification()
-EOR = 1
+
+#Specify extend of reaction (EOR)
+EOR = .99
 
 #Starting material mass and moles
-a.mass = 92.09382
-b.mass = 564.94
+a.mass = 285.52
+b.mass = 600
 a.mol = round(a.mass / a.mw, 3) * 1000
 b.mol = round(b.mass / b.mw, 3) * 1000
 unreacted = b.mol - (EOR * b.mol)
 b.mol = EOR * b.mol
 bms = b.mol
 
-
-#Define limiting reagent
-try:
-    if len(a.comp) >= len(b.comp):
-        species = len(a.comp)
-except TypeError:
-    species = len(a.comp)
-
 #Creates final product name(s) from starting material name(s)
 final_product_names = [a.sn, b.sn]
-final_product_names.extend([f"{a.sn}({1})_{b.sn}({str(i)})" for i in range(1, species + 1)])
+final_product_names.extend([f"{a.sn}({1})_{b.sn}({str(i)})" for i in range(1, 1001)])
 
 #Creates final product molar masses from final product name(s)
 final_product_masses = ({a.sn: round(a.mw, 1), b.sn: round(b.mw, 1)})
-final_product_masses.update({f"{a.sn}({1})_{b.sn}({str(i)})": round(a.mw + i * b.mw - i * rt.wl, 1) for i in range(1, species + 1)})
-
-#Creates starting molar amounts from final product names
-starting_molar_amounts = ({a.sn: [a.mol], b.sn: [b.mol]})
-starting_molar_amounts.update({f"{a.sn}({1})_{b.sn}({str(i)})": [0] for i in range(1, species + 1)})
-
-#Creates finish molar amounts from final product names
-final_molar_amounts = ({a.sn: [0], b.sn: [0]})
-final_molar_amounts.update({f"{a.sn}({1})_{b.sn}({str(i)})": [0] for i in range(1, species + 1)})
+final_product_masses.update({f"{a.sn}({1})_{b.sn}({str(i)})": round(a.mw + i * b.mw - i * rt.wl, 1) for i in range(1, 1001)})
 
 #Specifty rate constants
-k1 = 1
-k2 = .5
+prgK = 1
+srgK = 1
+cgK = .0
 
 #Creats starting composition list
 composition = []
-for i in range(0, int(a.mol)):
-    composition.extend(group.__name__ for group in a.comp)
+try:
+    for i in range(0, int(a.mol)):
+        composition.extend(group for group in a.compmw)
+except TypeError:
+    for i in range(0, int(a.mol)):
+        composition.append(a.mw)
 
-
-#Creates weights from starting commposition list
+#Create weights from starting composition list
 weights = []
 for group in composition:
-    if group == a.prg.__name__:
-        weights.append(k1)
+    if group == a.prgmw:
+        weights.append(prgK)
     else:
-        weights.append(k2)
+        weights.append(srgK)
 
 #Reacts away b.mol until gone.  Still need to add different rate constants(weights)
 while b.mol >= 0:
     MC = random.choices(list(enumerate(composition)), weights=weights, k=1)[0]
-    if MC[1] != rt.rp.__name__:
-        composition[MC[0]] = rt.rp.__name__
+    if MC[1] != a.prgmw or MC[1] != a.srgmw:
+        composition[MC[0]] = round(MC[1] + b.mw - rt.wl, 4)
         b.mol -= 1
-        weights[MC[0]] = 0
+        weights[MC[0]] = cgK
     else:
         pass
     print(round(100-(b.mol/bms*100), 2))
 
 #Seperates composition into compounds
-composition = [composition[x:x+len(a.comp)] for x in range(0, len(composition), len(a.comp))]
-composition_tuple = [tuple(l) for l in composition]
+try:
+    composition = [composition[x:x+len(a.comp)] for x in range(0, len(composition), len(a.comp))]
+    composition_tuple = [tuple(l) for l in composition]
+except TypeError:
+    composition = [composition[x:x+1] for x in range(0, len(composition), 1)]
+    composition_tuple = [tuple(l) for l in composition]
 
-#tabulates results from reacion
+#Tabulates final composition and converts to dataframe
 rxn_summary = collections.Counter(composition_tuple)
-
-#Convert rxn_summary to dataframe
-rxn_summary_df = pandas.DataFrame.from_dict(rxn_summary, orient='index')
+RS = []
+for key in rxn_summary:
+    MS = round(sum(key), 1)
+    for item in final_product_masses:
+        if MS == final_product_masses[item]:
+            RS.append((item, rxn_summary[key]))
+rxn_summary_df = pandas.DataFrame(RS, columns=['Product', 'Count'])
+rxn_summary_df.set_index('Product', inplace=True)
 rxn_summary_df.loc[f"{b.sn}"] = [unreacted]
 
-#Multiple datafrome rows by final product molar masses and add to new column
-rxn_summary_df['Final Product Mass'] = rxn_summary_df.index.map(final_product_masses.get)
-
-
-
-
+#Add colums to dataframe
+rxn_summary_df['Molar Mass'] = rxn_summary_df.index.map(final_product_masses.get)
+rxn_summary_df.sort_values(by=['Molar Mass'], ascending=True, inplace=True)
+rxn_summary_df['Mass'] = rxn_summary_df['Molar Mass'] * rxn_summary_df['Count']
+rxn_summary_df['(%)'] = round(rxn_summary_df['Mass'] / rxn_summary_df['Mass'].sum() * 100, 4)
 
 
 
 print(rxn_summary_df)
+
+
+
 
 
 #---------------------------------------------------User-Interface----------------------------------------------#
