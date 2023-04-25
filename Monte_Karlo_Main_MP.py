66984ffcda07573072f325652270f72a3592d09f
akarl2 <andrew.m.karl@gmail.com>
2023-04-25 06:47:25 -0500

    Added C8 - C18 Fatty Alcohol to Database

--- Monte_Karlo_Main_MP.py
+++ Monte_Karlo_Main_MP.py
@@ -717,21 +717,15 @@ def initialize_sim(workers):
     global total_ct, sn_dict, starting_mass, total_ct_sec, starting_mass_sec, end_metric_value, end_metric_value_sec, RXN_EM_2_Active_status, end_metric_selection, end_metric_selection_sec, starting_materials, \
         starting_materials_sec, total_samples
     starting_mass, starting_mass_sec, total_ct, total_ct_sec = 0, 0, 0, 0
+    row_for_sec = RXN_EM_Entry_2_SR.current()
     try:
         end_metric_value = float(RXN_EM_Value.get())
-    except ValueError:
-        messagebox.showinfo("Error", "Please enter a valid number for the end metric value")
-        return "Error"
-    RXN_EM_2_Active_status = RXN_EM_2_Active.get()
-    if RXN_EM_2_Active_status:
-        try:
+        RXN_EM_2_Active_status = RXN_EM_2_Active.get()
+        if RXN_EM_2_Active_status:
             end_metric_value_sec = float(RXN_EM_Value_2.get())
-        except ValueError:
-            messagebox.showinfo("Error", "Please enter a valid number for the end metric value")
-            return "Error"
-    row_for_sec = RXN_EM_Entry_2_SR.current()
-    if RXN_EM_2_Active_status:
-        end_metric_value_sec = float(RXN_EM_Value_2.get())
+    except ValueError:
+        messagebox.showinfo("Error", "Please enter a valid number for the end metric value(s)")
+        return "Error"
     else:
         end_metric_value_sec = 0
     end_metric_selection, end_metric_selection_sec = str(RXN_EM.get()), str(RXN_EM_Entry_2.get())
