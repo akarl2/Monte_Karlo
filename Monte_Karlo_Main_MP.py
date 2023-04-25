25d72655f204452b580374ab50c3629928270353
akarl2 <andrew.m.karl@gmail.com>
2023-04-24 21:04:37 -0500

    Beta version of updaing GUI w/ MP

--- Monte_Karlo_Main_MP.py
+++ Monte_Karlo_Main_MP.py
@@ -716,10 +716,20 @@ def clear_last():
 def initialize_sim(workers):
     global total_ct, sn_dict, starting_mass, total_ct_sec, starting_mass_sec, end_metric_value, end_metric_value_sec, RXN_EM_2_Active_status, end_metric_selection, end_metric_selection_sec, starting_materials, \
         starting_materials_sec, total_samples
-    row_for_sec = RXN_EM_Entry_2_SR.current()
     starting_mass, starting_mass_sec, total_ct, total_ct_sec = 0, 0, 0, 0
-    end_metric_value = float(RXN_EM_Value.get())
+    try:
+        end_metric_value = float(RXN_EM_Value.get())
+    except ValueError:
+        messagebox.showinfo("Error", "Please enter a valid number for the end metric value")
+        return "Error"
     RXN_EM_2_Active_status = RXN_EM_2_Active.get()
+    if RXN_EM_2_Active_status:
+        try:
+            end_metric_value_sec = float(RXN_EM_Value_2.get())
+        except ValueError:
+            messagebox.showinfo("Error", "Please enter a valid number for the end metric value")
+            return "Error"
+    row_for_sec = RXN_EM_Entry_2_SR.current()
     if RXN_EM_2_Active_status:
         end_metric_value_sec = float(RXN_EM_Value_2.get())
     else:
@@ -768,8 +778,7 @@ def initialize_sim(workers):
 
 def multiprocessing_sim():
     if __name__ == "__main__":
-        Buttons.Simulate.config(text="Running...")
-        Buttons.Simulate.config(state="disabled")
+        Buttons.Simulate.config(state="disabled", text="Running...")
         sim.progress['value'] = 0
         sim.progress_2['value'] = 0
         global running
@@ -777,6 +786,9 @@ def multiprocessing_sim():
         #workers = 1
         workers = int(os.cpu_count() * .85)
         initialize_sim(workers)
+        if initialize_sim(workers) == "Error":
+            Buttons.Simulate.config(text="Simulate", state="normal")
+            return
         progress_queue = multiprocessing.Manager().Queue()
         progress_queue_sec = multiprocessing.Manager().Queue()
         PID_list = multiprocessing.Manager().list()
